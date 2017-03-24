591c8b6db361bf8d8c206cbfcf3c8dd0e768ab82
hennayakaCM <chathura.hennayaka@gmail.com>
2017-03-24 09:50:39 +0530

    first commit

---
+++ src/main/java/quick/snb/App.java
@@ -0,0 +1,13 @@
+package quick.snb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
